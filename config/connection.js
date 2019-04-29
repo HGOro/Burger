8c7c80a2ff7ce47576632720146b82e5eeb0b0d4
HG_ORO <h.g.orozco@gmail.com>
2019-04-29 13:09:01 -0700

    put the JAWSDB connection back

--- config/connection.js
+++ config/connection.js
@@ -3,30 +3,29 @@ var mysql = require("mysql");
 
 //console.log("something")
 
- var connection = mysql.createConnection({
-   host: "localhost",
-   port: 3306,
-   user: "root",
-   password: "password",
-   database: "burgers_db"
- });
-
-//var connection
-
-//if (process.env.JAWSDB_URL) {
-//  connection = mysql.createConnection(process.env.JAWSDB_URL)
-//} else {
-//  connection = mysql.createConnection({
+//  var connection = mysql.createConnection({
 //    host: "localhost",
 //    port: 3306,
 //    user: "root",
 //    password: "password",
 //    database: "burgers_db"
-//  })
-//}
+//  });
+
+var connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  })
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
